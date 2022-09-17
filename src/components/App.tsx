73613271c58d3fe9f7171341624f40896f3a9ea0
Paulo Henrique Evangelista <phfernandesev@gmail.com>
2022-09-17 18:20:06 -0300

    Add countdown timer

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Countdown from 'components/Countdown';
 
 const App: React.FC = () => {
+  const targetDateInMilliseconds = Math.floor(new Date('Fri Oct 14 2022 11:00:00') as any);
+
   return (
     <div className="App">
-      test
+      <Countdown eventTime={targetDateInMilliseconds} />
     </div>
   );
 };
